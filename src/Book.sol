2afc3c798217905b77012758de50051024614020
jaredborders <44350516+JaredBorders@users.noreply.github.com>
2025-03-09 00:36:27 -0500

    👷 update __placeBid()

--- src/Book.sol
+++ src/Book.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {Clearinghouse} from "./Clearinghouse.sol";
 import {ERC20 as Synth} from "./ERC20.sol";
-import {Price} from "./Price.sol";
 import {Queue} from "./Queue.sol";
 
 /// @title minimal limit order book logic
@@ -13,10 +11,8 @@ contract Book {
 
     using Queue for Queue.T;
 
-    Clearinghouse clearinghouse;
-
-    Synth numeraire;
-    Synth index;
+    Synth immutable numeraire;
+    Synth immutable index;
 
     enum KIND {
         MARKET,
@@ -37,16 +33,20 @@ contract Book {
     }
 
     struct Trade {
-        address trader;
         KIND kind;
         SIDE side;
-        Price price;
+        uint256 price;
         uint256 quantity;
     }
 
     struct Order {
         uint256 id;
-        Trade trade;
+        address trader;
+        STATUS status;
+        KIND kind;
+        SIDE side;
+        uint256 price;
+        uint256 quantity;
         uint256 remaining;
     }
 
@@ -57,254 +57,225 @@ contract Book {
         Queue.T asks;
     }
 
-    uint256 private id;
+    /// @notice unique identifier for each order
+    /// @dev incremented for each new order
+    /// @custom:cid acronym for "current identifier"
+    uint256 private cid;
 
-    mapping(Price tick => Level level) internal levels;
-    mapping(uint256 index => Order order) internal orders;
-    mapping(uint256 index => STATUS status) internal statuses;
-    mapping(uint256 index => address trader) internal traders;
-    mapping(address trader => uint256[] indices) internal trades;
+    mapping(uint256 id => Order) internal orders;
+    mapping(uint256 price => Level) internal levels;
+    mapping(uint256 id => address trader) internal traders;
+    mapping(address trader => uint256[] ids) internal trades;
 
-    constructor(address clearinghouse_, address numeraire_, address index_) {
-        clearinghouse = Clearinghouse(clearinghouse_);
+    constructor(address numeraire_, address index_) {
         numeraire = Synth(numeraire_);
         index = Synth(index_);
     }
 
-    function depth(Price tick_)
+    function depth(uint256 price_)
         public
         view
         returns (uint256 bids, uint256 asks)
     {
-        Level storage level = levels[tick_];
+        Level storage level = levels[price_];
         return (level.bidDepth, level.askDepth);
     }
 
     function place(Trade calldata trade_) public {
-        // create reference to price level for given trade price
-        Level storage level = levels[trade_.price];
+        if (trade_.quantity == 0) revert("Invalid quantity");
+        if (trade_.price == 0) revert("Invalid price");
 
-        /// @custom:market orders are fill-or-kill; no partial fills
-        if (trade_.kind == KIND.MARKET) {
-            uint256 quantity = trade_.quantity;
-            uint256 price = Price.unwrap(trade_.price);
-
-            /// @dev to fill, a bid must be matched with an ask
-            if (trade_.side == SIDE.BID) {
-                uint256 desired = quantity / price;
-                if (level.askDepth >= desired) {
-                    while (desired > 0) {
-                        // peek at ask order at front of queue
-                        Order storage ask = orders[level.asks.peek()];
-
-                        if (desired >= ask.remaining) {
-                            /// @custom:desired update ->
-                            // desired: desired - ask.remaining
-
-                            /// @custom:order update ->
-                            // orders: ask status to FILLED
-                            // orders: ask remaining to 0
-
-                            /// @custom:level update ->
-                            // level: ask depth
-                            // level: dequeue ask order
-
-                            /// @custom:settlement
-                            // index tokens --> trader who placed market bid
-                            // numeraire tokens --> trader who placed ask
-                        } else {
-                            /// @custom:desired update ->
-                            // desired: 0
-
-                            /// @custom:order update ->
-                            // orders: ask status to PARTIAL
-                            // orders: ask remaining to ask.remaining - desired
-
-                            /// @custom:level update ->
-                            // level: ask depth
-
-                            /// @custom:settlement
-                            // index tokens --> trader who placed market bid
-                            // numeraire tokens --> trader who placed ask
-                        }
-                    }
-                }
-            }
-
-            /// @dev to fill, an ask must be matched with a bid
-            if (trade_.side == SIDE.ASK) {
-                uint256 desired = quantity * price;
-                if (level.bidDepth >= desired) {
-                    while (desired > 0) {
-                        // peek at bid order at front of queue
-                        Order storage bid = orders[level.bids.peek()];
-
-                        if (desired >= bid.remaining) {
-                            /// @custom:desired update ->
-                            // desired: desired - bid.remaining
-
-                            /// @custom:order update ->
-                            // orders: bid status to FILLED
-                            // orders: bid remaining to 0
-
-                            /// @custom:level update ->
-                            // level: bid depth
-                            // level: dequeue bid order
-
-                            /// @custom:settlement
-                            // numeraire tokens --> trader who placed market ask
-                            // index tokens --> trader who placed bid
-                        } else {
-                            /// @custom:desired update ->
-                            // desired: 0
-
-                            /// @custom:order update ->
-                            // orders: bid status to PARTIAL
-                            // orders: bid remaining to bid.remaining - desired
-
-                            /// @custom:level update ->
-                            // level: bid depth
-
-                            /// @custom:settlement
-                            // numeraire tokens --> trader who placed market ask
-                            // index tokens --> trader who placed bid
-                        }
-                    }
-                }
-            }
-        }
-        
-        /// @custom:limit orders are filled (partially or fully) when possible
         if (trade_.kind == KIND.LIMIT) {
             if (trade_.side == SIDE.BID) {
-                level.bids.enqueue(id);
-                level.bidDepth += trade_.quantity;
-            }
-
-            if (trade_.side == SIDE.ASK) {
-                level.asks.enqueue(id);
-                level.askDepth += trade_.quantity;
+                __placeBid(trade_);
+            } else {
+                __placeAsk(trade_);
             }
         }
 
-        id++;
-    }
-
-    function remove(uint256 orderId_) public {
-        require(traders[orderId_] == msg.sender, "Not order owner");
-
-        STATUS status = statuses[orderId_];
-
-        require(
-            status == STATUS.OPEN || status == STATUS.PARTIAL,
-            "Order not removable"
-        );
-
-        Order storage order = orders[orderId_];
-        Level storage level = levels[order.trade.price];
-
-        statuses[orderId_] = STATUS.CANCELLED;
-
-        if (order.trade.kind == KIND.LIMIT) {
-            if (order.trade.side == SIDE.BID) {
-                // Update bid depth
-                level.bidDepth -= order.remaining;
-
-                // Refund remaining numeraire tokens
-                clearinghouse.transfer(
-                    numeraire, order.remaining, address(this), msg.sender
-                );
-            }
-
-            if (order.trade.side == SIDE.ASK) {
-                // Update ask depth
-                level.askDepth -= order.remaining;
-
-                // Refund remaining index tokens
-                clearinghouse.transfer(
-                    index, order.remaining, address(this), msg.sender
-                );
+        if (trade_.kind == KIND.MARKET) {
+            if (trade_.side == SIDE.BID) {
+                revert("Unsupported");
+            } else {
+                revert("Unsupported");
             }
         }
     }
 
-    function fill(Trade calldata trade_) public view {}
+    function remove(uint256 id_) public {
+        require(traders[id_] == msg.sender, "Not order owner");
+        require(orders[id_].status != STATUS.FILLED, "Order filled");
+        require(orders[id_].status != STATUS.CANCELLED, "Order cancelled");
+        require(orders[id_].kind != KIND.MARKET, "Market order");
 
-    function fill(uint256 orderId_) public view {
-        Order storage order = orders[orderId_];
-        Level storage level = levels[order.trade.price];
+        orders[id_].status = STATUS.CANCELLED;
 
-        if (order.trade.side == SIDE.BID) {
-            // fill bid order with ask order(s)
-            // - bid order status must be FILLED
-            // - ask order status must be FILLED unless last ask order
-            // - last ask order can be PARTIAL or FILLED
-            do {
-                uint256 askId = level.asks.peek();
+        Order storage order = orders[id_];
+        Level storage level = levels[order.price];
+
+        // cache remaining quantity of order
+        uint256 remaining = order.remaining;
+
+        // zero out remaining quantity of order
+        order.remaining = 0;
+
+        if (order.side == SIDE.BID) {
+            // reduce bid depth of current price level
+            level.bidDepth -= remaining;
+
+            /// @custom:settle remaining numeraire tokens
+            numeraire.transfer(msg.sender, remaining);
+        } else {
+            //  reduce ask depth of current price level
+            level.askDepth -= remaining;
+
+            /// @custom:settle remaining index tokens
+            index.transfer(msg.sender, remaining);
+        }
+    }
+
+    // while there are asks at this price level,
+    // fill the bid order with the ask order(s)
+    // until the bid order is filled.
+    //
+    // if bid order was filled partially, update the bid order's
+    // remaining quantity and enqueue bid order at this price level.
+    //
+    // if bid order was filled completely, update the bid order's
+    // status to FILLED and do not enqueue bid order at this price level.
+    //
+    // if there are no asks at this price level, enqueue bid order.
+    function __placeBid(Trade calldata trade_) private returns (uint256 id) {
+        /// @dev immediately transfer numeraire tokens to contract
+        numeraire.transferFrom(msg.sender, address(this), trade_.quantity);
+
+        // create reference to price level in contract storage
+        Level storage level = levels[trade_.price];
+
+        // assign bid order id to current identifier then increment
+        id = cid++;
+
+        /// @notice create new bid order (in memory)
+        /// @dev bid status is OPEN until filled or cancelled
+        /// @dev initially, remaining quantity is equal to total quantity
+        Order memory bid = Order({
+            id: id,
+            trader: msg.sender,
+            status: STATUS.OPEN,
+            kind: trade_.kind,
+            side: trade_.side,
+            price: trade_.price,
+            quantity: trade_.quantity,
+            remaining: trade_.quantity
+        });
+
+        uint256 p = bid.price;
+        uint256 n = bid.quantity;
+        uint256 i = n / p;
+
+        while (!level.asks.isEmpty()) {
+            uint256 askId = level.asks.peek();
+            uint256 askRemaining = orders[askId].remaining;
+
+            if (orders[askId].status == STATUS.CANCELLED) {
+                level.asks.dequeue();
+                continue;
+            }
+
+            if (i >= askRemaining) {
+                // define amount of ask order filled
+                uint256 askFilled = askRemaining;
+
+                // decrement (i) to reflect bid quantity filled
+                i -= askFilled;
+
+                // reduce ask depth of current price level
+                level.askDepth -= askFilled;
+
+                // create reference to ask order
                 Order storage ask = orders[askId];
 
-                /// @custom:todo
+                // update ask order status to FILLED
+                ask.status = STATUS.FILLED;
 
-                if (__compareQuantity(order, ask) == 0) break;
-                if (__compareQuantity(order, ask) == -1) break;
-                if (__compareQuantity(order, ask) == 1) break;
+                // update ask order remaining quantity to 0
+                ask.remaining -= askFilled;
 
-                // filled ask orders must be dequeued/removed
-                // if partial ask order:
-                // - update ask order quantity
-                // - update ask order status to PARTIAL
-                // - do not dequeue ask order
+                // update bid order remaining quantity
+                bid.remaining -= askFilled * p;
 
-                // ensure ask depth is updated following every fill
-            } while (true);
+                // update bid order status to PARTIAL
+                bid.status = STATUS.PARTIAL;
+
+                /// @custom:settle
+                numeraire.transfer(ask.trader, askFilled * p);
+                index.transfer(msg.sender, askFilled);
+
+                /// @custom:dequeue ask order
+                level.asks.dequeue();
+            } else {
+                // define amount of ask order filled
+                uint256 askFilled = i;
+
+                // decrement (i) to reflect bid quantity filled
+                i -= askFilled;
+
+                // reduce ask depth of current price level
+                level.askDepth -= askFilled;
+
+                // create reference to ask order
+                Order storage ask = orders[askId];
+
+                // ensure ask order status is PARTIAL
+                ask.status = STATUS.PARTIAL;
+
+                // update ask order remaining quantity
+                ask.remaining -= askFilled;
+
+                // update bid order remaining quantity
+                bid.remaining -= askFilled * p;
+
+                // update bid order status to FILLED
+                bid.status = STATUS.FILLED;
+
+                /// @custom:settle
+                numeraire.transfer(ask.trader, askFilled * p);
+                index.transfer(msg.sender, askFilled);
+
+                break;
+            }
         }
 
-        if (order.trade.side == SIDE.ASK) {
-            // fill ask order with bid order(s)
-            // - ask order status must be FILLED
-            // - bid order status must be FILLED unless last bid order
-            // - last bid order can be PARTIAL or FILLED
-            do {
-                uint256 bidId = level.bids.peek();
-                Order storage bid = orders[bidId];
-
-                /// @custom:todo
-
-                if (__compareQuantity(bid, order) == 0) break;
-                if (__compareQuantity(bid, order) == -1) break;
-                if (__compareQuantity(bid, order) == 1) break;
-
-                // filled bid orders must be dequeued/removed
-                // if partial bid order:
-                // - update bid order quantity
-                // - update bid order status to PARTIAL
-                // - do not dequeue bid order
-
-                // ensure bid depth is updated following every fill
-            } while (true);
+        /// @dev if bid not fully filled, enqueue bid order
+        if (bid.status != STATUS.FILLED) {
+            level.bids.enqueue(id);
         }
 
-        // if filled order is market order:
-        // - update order status to FILLED
-        // - do not dequeue order (it was never enqueued)
+        // update bid depth of current price level
+        level.bidDepth += bid.remaining;
 
-        // if filled order is limit order:
-        // - update order status to FILLED
-        // - dequeue order
+        // add storage reference to bid order
+        orders[id] = bid;
+
+        // add storage reference to trader responsible for bid order
+        traders[id] = msg.sender;
+
+        // add order id to the history of trades made by trader
+        trades[msg.sender].push(id);
     }
 
-    /// @custom:todo
-    function __compareQuantity(
-        Order memory bid_,
-        Order memory ask_
-    )
-        private
-        pure
-        returns (int256)
-    {
-        if (bid_.trade.quantity < ask_.trade.quantity) return -1;
-        if (bid_.trade.quantity > ask_.trade.quantity) return 1;
-        return 0;
+    function __placeAsk(Trade calldata trade_) private returns (uint256 id) {
+        /// @dev immediately transfer index tokens to contract
+        index.transferFrom(msg.sender, address(this), trade_.quantity);
+
+        // create reference to price level in contract storage
+        Level storage level = levels[trade_.price];
+
+        // assign ask order id to current identifier then increment
+        id = cid++;
+
+        if (level.bids.isEmpty()) revert("Unsupported");
+        else revert("Unsupported");
     }
 
 }
