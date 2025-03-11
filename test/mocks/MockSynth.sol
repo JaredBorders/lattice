e417f714f0f85e2e468e235c03d2cd9d227db2ba
jaredborders <44350516+JaredBorders@users.noreply.github.com>
2025-03-11 10:33:56 -0400

    🎭 update synth mock

--- test/mocks/MockSynth.sol
+++ test/mocks/MockSynth.sol
@@ -4,8 +4,14 @@ pragma solidity 0.8.28;
 import {ERC20} from "../../src/ERC20.sol";
 
 /// @title simplified mock synth token for testing
+/// @author jaredborders
+/// @author flocast
+/// @custom:version v0.0.1
 contract MockSynth is ERC20 {
 
+    /// @notice construct a new tokenized mock synth
+    /// @param name_ name of the token
+    /// @param symbol_ symbol of the token
     constructor(
         string memory name_,
         string memory symbol_
@@ -13,9 +19,10 @@ contract MockSynth is ERC20 {
         ERC20(name_, symbol_)
     {}
 
-    // Public mint function - anyone can mint tokens for testing purposes
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    /// @notice allows any caller to mint tokens
+    /// @dev for testing purposes only
+    function mint(address to_, uint256 amount_) public {
+        _mint(to_, amount_);
     }
 
 }
